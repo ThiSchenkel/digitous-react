bc428b9221a3f1483093f7495080226bc2b97157
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-25 16:17:05 +0100

    Variable globale ou bien.. grrrr

--- react/jour1/elsy/src/App.js
+++ react/jour1/elsy/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import './App.css';
 
-const tempMin = -20;
-const tempMax = 40;
-const heartMin = 80;
-const heartMax = 180;
-const stepsMin = 0;
-const stepsMax = 50000;
+
 export class App extends React.Component {
+  tempMin = -20;
+  tempMax = 40;
+  heartMin = 80;
+  heartMax = 180;
+  stepsMin = 0;
+  stepsMax = 50000;
   render() {
     return (
-      <div>
-
+      <div className="container-fluid" >
+        <div className="row">
+          <p>Heart : {this.heartMin}</p>
+          <p>Temperature : {this.tempMin}</p>
+          <p>Steps : {this.stepsMin}</p>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
