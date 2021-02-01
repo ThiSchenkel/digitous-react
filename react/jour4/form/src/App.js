e66bc5247686c39f3aab34f976346a9de136cf45
ThiSchenkel <thi.schenkel@gmail.com>
2021-02-01 16:55:30 +0100

    Working on it

--- react/jour4/form/src/App.js
+++ react/jour4/form/src/App.js
@@ -4,24 +4,37 @@ import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mail: "",
+      passeword: "",
+      value: "",
+    }
+  }
+
+  handleChange = (event) => {
+    console.log(event);
+  }
+
+
   render() {
     return (
       <div className="container">
         <form >
           <h1>Login</h1>
-          <label> Email Adress
-          <input type="text" mail="mail" />
-          </label>
-          <label> Passeword
-            <input type="text" passeword="passeword" />
-          </label>
+
+          <p>Email Adress</p>
+          <input type="text" mail={this.state.mail} onChange={this.handleChange} />
+
+          <p>Passeword</p>
+          <input type="text" passeword={this.state.passeword} onChange={this.handleChange} />
+
           <p>Remember Me</p>
           <input type="submit" value="Submit" />
         </form>
       </div>
-
     );
   }
 }
-
 export default App;
