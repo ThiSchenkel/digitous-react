c1adc33a880e40e4869092e41519f92af3d33840
ThiSchenkel <thi.schenkel@gmail.com>
2021-02-01 15:16:25 +0100

    Start exo

--- react/jour4/form/src/App.js
+++ react/jour4/form/src/App.js
@@ -6,22 +6,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <form >
+          <h1>Login</h1>
+          <label> Email Adress
+          <input type="text" mail="mail" />
+          </label>
+          <label> Passeword
+            <input type="text" passeword="passeword" />
+          </label>
+          <p>Remember Me</p>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
+
     );
   }
 }
