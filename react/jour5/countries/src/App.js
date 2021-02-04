a3bffbfb2254199b165695f96329f0f667c139ef
ThiSchenkel <thi.schenkel@gmail.com>
2021-02-04 16:07:25 +0100

    I did it! but not finish!

--- react/jour5/countries/src/App.js
+++ react/jour5/countries/src/App.js
@@ -17,26 +17,30 @@ class App extends React.Component {
   componentDidMount = () => {
     fetch("https://restcountries.eu/rest/v2/name/france")
       .then(response => response.json())
-      .then(name => {
-        this.setState({
-          name: name.name
-        }); console.log(name[0].capital)
+      .then(country => {
+        this.setState({ name: country[0].name })
+        this.setState({ capital: country[0].capital })
+        this.setState({ region: country[0].region })
+        this.setState({ population: country[0].population })
+        this.setState({ flag: country[0].flag })
       });
   }
 
 
-
   render() {
-    const { name, capital, flag, region } = this.state;
+
     return (
       <div className="container">
-
         <h1>Country Selector</h1>
         <div className="row ">
           <div className="col col-6 card" style={{ width: "18rem" }}>
-            <img className="card-img-top" />
+            <p className="card-text">Flag : {this.state.flag}</p>
             <div className="card-body">
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p className="card-text">Country : {this.state.name}</p>
+              <p className="card-text">Capital : {this.state.capital}</p>
+              <p className="card-text">Region : {this.state.region}</p>
+              <p className="card-text">Population : {this.state.population}</p>
+
             </div>
           </div>
         </div>
