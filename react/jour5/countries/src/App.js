4efc30d25b466ff69b418d07fa41741d4d4fdcd2
ThiSchenkel <thi.schenkel@gmail.com>
2021-02-04 12:31:14 +0100

    Try something

--- react/jour5/countries/src/App.js
+++ react/jour5/countries/src/App.js
@@ -3,10 +3,42 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      capital: "",
+      flag: "",
+      population: "",
+      region: ""
+    }
+  }
 
+  componentDidMount = () => {
+    fetch("https://restcountries.eu/rest/v2/name/france")
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          name: data.name,
+          capital: data.capital,
+          flag: data.flag,
+          region: data.region
+        })
+      }
+      )
+  }
+
+
+  render() {
+    const { countries } = this.componentDidMount;
+    return (
+      <div className="container">
+        <div className="row text-center">
+          <h1>Country Selector</h1>
+          <ul>
+            {countries.map(country => <li key={country.objectID}></li>)}
+          </ul>
+        </div>
       </div>
     );
   }
