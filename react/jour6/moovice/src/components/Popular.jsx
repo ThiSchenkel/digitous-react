2d5682b6cee089f99217e5c853138c3d587a3545
ThiSchenkel <thi.schenkel@gmail.com>
2021-02-10 18:13:42 +0100

    Working on it

--- react/jour6/moovice/src/components/Popular.jsx
+++ react/jour6/moovice/src/components/Popular.jsx
@@ -5,6 +5,26 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 
 class Popular extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: [""]
+        }
+    }
+
+    componentDidMount = (movies) => {
+        fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=24f4ec427a73c8c53d9cb5a30964b4d8`)
+            .then((response) => response.json())
+            .then((response) => {
+                this.setState({
+                    movies: response.movies
+                }); console.log(movies)
+            })
+
+    }
+
+
+
     render() {
         return (
             <div className="container">
